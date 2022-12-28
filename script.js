9cf2ca13e18ce0eae8b584cb44409ad2349a0c80
Yet <78223322+Yetity@users.noreply.github.com>
2022-12-28 10:47:49 +0000

    Got working line graph

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+var xValues = [];
+var yValues = [];
+generateData("Math.cos(x)", 0, 100, 0.5);
 
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      pointRadius: 1,
+      borderColor: "rgba(255,0,0,0.5)",
+      data: yValues
+    }]
+  },    
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "y = x*x/x",
+      fontSize: 16
+    }
+  }
+});
+function generateData(value, i1, i2, step = 1) {
+  for (let x = i1; x <= i2; x += step) {
+    yValues.push(eval(value));
+    xValues.push(x);
+  }
+}
